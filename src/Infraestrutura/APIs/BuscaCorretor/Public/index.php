198991ac2398fbbf63992aa9b4db7e717b0b678c
19950512 <mattmaydana@gmail.com>
2025-02-20 09:34:39 -0300

    CORS

--- src/Infraestrutura/APIs/BuscaCorretor/Public/index.php
+++ src/Infraestrutura/APIs/BuscaCorretor/Public/index.php
@@ -6,6 +6,16 @@ declare(strict_types=1);
 
 use App\Infraestrutura\APIs\Router;
 
+header("Access-Control-Allow-Origin: https://buscacreci.com.br"); // Permitir apenas buscacreci.com.br
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Métodos permitidos
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeçalhos permitidos
+
+// Se for uma requisição OPTIONS (preflight), responde com 200 e encerra
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 $container = require __DIR__ . '/../../../../Aplicacao/Compartilhado/Container.php';
 
 new Router(
